b8ce03fe2aae86c01f4195a1c14b3967d12ed5e3
Owen Zheng <johndoe@example.com>
2021-11-09 09:31:38 -0800

    Tree Insert Method Implementation

---
+++ src/DataStructures/Heaps/PriorityQueueWithHeap.java
@@ -0,0 +1,17 @@
+package DataStructures.Heaps;
+
+public class PriorityQueueWithHeap {
+    private Heaps heap = new Heaps();
+
+    public void enqueue(int item) {
+        heap.insert(item);
+    }
+
+    public int dequeue() {
+        return heap.remove();
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import DataStructures.AVLTrees.AVLTree;
-import DataStructures.BinaryTrees.Tree;
+import DataStructures.Heaps.PriorityQueueWithHeap;
+
 
 public class Main {
     public static void main (String[] arg) {
-        Tree tree = new Tree();
-        tree.insert(10);
-        tree.insert(5);
-        tree.insert(12);
 
-
-
-        System.out.println(tree.isPerfect());
     }
 }
 
